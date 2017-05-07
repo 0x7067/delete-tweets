2fa2232dcad6a78c719451a85b16c5bc078dfbe8
0x7067 <moisespedro15@gmail.com>
2017-05-07 02:04:34 -0300

    Code is now PEP8 compliant

--- delete_all_tweets.py
+++ delete_all_tweets.py
@@ -32,23 +32,27 @@ consumer_secret = config["twitter"]["consumer_secret"]
 access_key = config["twitter"]["access_key"]
 access_secret = config["twitter"]["access_secret"]
 
+
 def deleteThread(api, objectId):
-	try:
-		api.destroy_status(objectId)
-		print "Deleted:", objectId
-	except:
-		print "Failed to delete:", objectId
+    try:
+        api.destroy_status(objectId)
+        print "Deleted:", objectId
+    except:
+        print "Failed to delete:", objectId
+
 
 def oauth_login(consumer_key, consumer_secret):
     """Authenticate with twitter using OAuth"""
-    
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth_url = auth.get_authorization_url()
-    
-    verify_code = raw_input("Authenticate at %s and then enter you verification code here > " % auth_url) 
+
+    verify_code = raw_input(
+        "Authenticate at %s and then enter you verification code here > " % auth_url)
     auth.get_access_token(verify_code)
-    
+
     return tweepy.API(auth)
+
 
 def batch_delete(api):
     print "You are about to Delete all tweets from the account @%s." % api.verify_credentials().screen_name
@@ -57,17 +61,17 @@ def batch_delete(api):
     if do_delete.lower() == 'yes':
         for status in tweepy.Cursor(api.user_timeline).items():
             try:
-                #api.destroy_status(status.id)
-                #print "Deleted:", status.id
-                thread.start_new_thread( deleteThread, (api, status.id, ) )
+                # api.destroy_status(status.id)
+                # print "Deleted:", status.id
+                thread.start_new_thread(deleteThread, (api, status.id, ))
             except:
                 print "Failed to delete:", status.id
 
 if __name__ == "__main__":
-    #authorize twitter, initialize tweepy
+    # authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
     print "Authenticated as: %s" % api.me().screen_name
-    
+
     batch_delete(api)
